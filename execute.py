2258cb8fc1b414f82a6fe3e35681c4d6b17b8c02
AbbasAlibhaiER <abbas.alibhai@extendrobotics.com>
2024-12-19 14:36:12 +0000

    attempt at fix for reboot issue

--- execute.py
+++ execute.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import subprocess
 import time
 import os
@@ -467,7 +469,7 @@ def run_job(job_id, job_document):
         # Schedule a reboot if specified in the job document
         if reboot_after_job:
             logging.info("Job complete. Scheduling device to restart in 15 seconds.")
-            subprocess.Popen("nohup sh -c 'sleep 15; shutdown -r now' &", shell=True)
+            subprocess.Popen("nohup sh -c 'sleep 15; shutdown -r now'", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
             time.sleep(1)
             return True, f"Job executed successfully. {message} Your device will reboot in the next 30 seconds."
 
